aa311633f0976386856e8ce9fd61d9d98b26c67f
MarcelRuoff <marcel.ruoff@gmx.de>
2018-11-06 17:08:07 +0100

    Inital Commit

---
+++ EchoWithCounterBot.cs
@@ -0,0 +1,187 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using IBM.WatsonDeveloperCloud.ToneAnalyzer.v3;
+using IBM.WatsonDeveloperCloud.ToneAnalyzer.v3.Model;
+using IBM.WatsonDeveloperCloud.Util;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bot_Builder_Echo_Bot_V41
+{
+    /// <summary>
+    /// Represents a bot that processes incoming activities.
+    /// For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
+    /// This is a Transient lifetime service.  Transient lifetime services are created
+    /// each time they're requested. For each Activity received, a new instance of this
+    /// class is created. Objects that are expensive to construct, or have a lifetime
+    /// beyond the single turn, should be carefully managed.
+    /// For example, the <see cref="MemoryStorage"/> object and associated
+    /// <see cref="IStatePropertyAccessor{T}"/> object are created with a singleton lifetime.
+    /// </summary>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-2.1"/>
+
+    
+
+    public class EchoWithCounterBot : IBot
+    {
+        private readonly EchoBotAccessors _accessors;
+        private readonly ILogger _logger;
+        private ToneChatInput toneChatInput = new ToneChatInput()
+        {
+            Utterances = new List<Utterance>(),
+        };
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EchoWithCounterBot"/> class.
+        /// </summary>
+        /// <param name="accessors">A class containing <see cref="IStatePropertyAccessor{T}"/> used to manage state.</param>
+        /// <param name="loggerFactory">A <see cref="ILoggerFactory"/> that is hooked to the Azure App Service provider.</param>
+        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1#windows-eventlog-provider"/>
+        public EchoWithCounterBot(EchoBotAccessors accessors, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null)
+            {
+                throw new System.ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<EchoWithCounterBot>();
+            _logger.LogTrace("EchoBot turn start.");
+            _accessors = accessors ?? throw new System.ArgumentNullException(nameof(accessors));
+        }
+
+        /// <summary>
+        /// Every conversation turn for our Echo Bot will call this method.
+        /// There are no dialogs used, since it's "single turn" processing, meaning a single
+        /// request and response.
+        /// </summary>
+        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
+        /// for processing this conversation turn. </param>
+        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
+        /// <seealso cref="BotStateSet"/>
+        /// <seealso cref="ConversationState"/>
+        /// <seealso cref="IMiddleware"/>
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Handle Message activity type, which is the main activity type for shown within a conversational interface
+            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
+            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                string credentials = string.Empty;
+
+                string endpoint = string.Empty;
+                string username = string.Empty;
+                string password = string.Empty;
+
+                endpoint = "https://gateway.watsonplatform.net/tone-analyzer/api";
+                username = "c33ec0e1-58de-4dbb-987c-0a425f983a84";
+                password = "5CsVdSN3ZXZn";
+                var versionDate = "2017-09-21";
+
+                ToneAnalyzerService toneAnalyzer = new ToneAnalyzerService(username, password, versionDate);
+
+                ToneInput toneInput = new ToneInput()
+                {
+                    Text = turnContext.Activity.Text,
+                };
+
+                Utterance input = new Utterance()
+                {
+                    Text = turnContext.Activity.Text,
+                    User = turnContext.Activity.From.Name,
+                };
+
+                toneChatInput.Utterances.Add(input);
+
+
+                var postToneResult = toneAnalyzer.ToneChat(toneChatInput, "application/json", null);
+
+                // string json_output = JsonConvert.SerializeObject(, Formatting.Indented);
+
+                // IList<Tone> tones = JsonConvert.DeserializeObject<IList<Tone>>(json_output);
+
+                // Get the conversation state from the turn context.
+                var state = await _accessors.CounterState.GetAsync(turnContext, () => new CounterState());
+
+                // Bump the turn count for this conversation.
+                state.TurnCount++;
+
+                // Set the property using the accessor.
+                await _accessors.CounterState.SetAsync(turnContext, state);
+
+                // Save the new turn count into the conversation state.
+                await _accessors.ConversationState.SaveChangesAsync(turnContext);
+
+                // Echo back to the state of the user.
+                string responseMessage = null;
+
+                    // foreach (Tone tone in tones)
+                foreach (UtteranceAnalysis utterance_Tone in postToneResult.UtterancesTone)
+                    {
+                        foreach (ToneChatScore tone in utterance_Tone.Tones)
+                        {
+                            if (tone.ToneId == "excited")
+                        {
+                            responseMessage += $"You feel '{tone.ToneName}' with a score of: {tone.Score}\n \U0001F917 \n";
+                        }
+
+                            if (tone.ToneId == "frustrated")
+                        {
+                            responseMessage += $"You feel '{tone.ToneName}' with a score of: {tone.Score}\n \U0001F612 \n";
+                        }
+
+                            if (tone.ToneId == "impolite")
+                        {
+                            responseMessage += $"You feel '{tone.ToneName}' with a score of: {tone.Score}\n \U0001F620 \n";
+                        }
+
+                            if (tone.ToneId == "polite")
+                        {
+                            responseMessage += $"You feel '{tone.ToneName}' with a score of: {tone.Score}\n \U0001F642 \n";
+                        }
+
+                            if (tone.ToneId == "sad")
+                        {
+                            responseMessage += $"You feel '{tone.ToneName}' with a score of: {tone.Score}\n \U0001F642 \n";
+                        }
+
+                            if (tone.ToneId == "satisfied")
+                        {
+                            responseMessage += $"You feel '{tone.ToneName}' with a score of: {tone.Score}\n \U0001F60A \n";
+                        }
+
+                            if (tone.ToneId == "sympathetic")
+                        {
+                            responseMessage += $"You feel '{tone.ToneName}' with a score of: {tone.Score}\n \U0001F600 \n";
+                        }
+                        }
+
+                        await turnContext.SendActivityAsync(responseMessage);
+                }
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+            }
+        }
+    }
+
+    public class Tone
+    {
+        public string Score { get; set; }
+
+        public string Tone_id { get; set; }
+
+        public string Tone_name { get; set; }
+    }
+}
